944217fb33aa0da4decd97574bf98e9f9b43cbb3
TN-2001 <penry7335@gmail.com>
2025-07-02 13:56:54 +0900

    remove: app_colorsとmain_ui_powerdropdownを削除、widget_testの中身をコメントアウト

--- lib/constants/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class AppColors {
-  static Color toolBaseColor = Color.fromARGB(255, 255, 255, 255);
-  static Color toolBorderColor = Color.fromARGB(255, 245, 245, 245);
-}
