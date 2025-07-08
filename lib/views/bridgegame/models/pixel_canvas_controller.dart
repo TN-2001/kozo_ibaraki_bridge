4058ba4b6806b0cf0dd6d98bdcdfa905c739d7ac
TN-2001 <penry7335@gmail.com>
2025-07-08 17:06:33 +0900

    remove: pixel_paint系の描画クラスを削除

--- lib/views/bridgegame/models/pixel_canvas_controller.dart
+++ lib/views/bridgegame/models/pixel_canvas_controller.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 class PixelCanvasController extends ChangeNotifier {
+
+  // パラメータ
   int _gridWidth = 16;
   int _gridHeight = 16;
-  Color selectedColor = Colors.black;
-  late List<Color> pixelColors;
-  final List<List<Color>> undoStack = [];
-  final List<List<Color>> redoStack = [];
+  Color _selectedColor = Colors.black;
+  late List<Color> _pixelColors;
+  final List<List<Color>> _undoStack = [];
+  final List<List<Color>> _redoStack = [];
 
   final TextEditingController widthController = TextEditingController(text: '16');
   final TextEditingController heightController = TextEditingController(text: '16');
 
-  // geter
+  // ゲッター
   int get gridWidth => _gridWidth;
   int get gridHeight => _gridHeight;
+  Color get selectedColor => _selectedColor;
+  Color getPixelColor(int index) {
+    if (index < 0 || index >= _pixelColors.length) {
+      // エラーで一時停止
+      throw RangeError('Index out of bounds: $index');
+    }
+    return _pixelColors[index];
+  }
 
 
   PixelCanvasController() {
@@ -21,36 +31,36 @@ class PixelCanvasController extends ChangeNotifier {
   }
 
   void _initCanvas() {
-    pixelColors = List.generate(_gridWidth * _gridHeight, (_) => const Color.fromARGB(0, 255, 255, 255));
-    undoStack.clear();
-    redoStack.clear();
+    _pixelColors = List.generate(_gridWidth * _gridHeight, (_) => const Color.fromARGB(0, 255, 255, 255));
+    _undoStack.clear();
+    _redoStack.clear();
     notifyListeners();
   }
 
   void saveToUndo() {
-    undoStack.add(List<Color>.from(pixelColors));
-    redoStack.clear();
+    _undoStack.add(List<Color>.from(_pixelColors));
+    _redoStack.clear();
   }
 
   void undo() {
-    if (undoStack.isNotEmpty) {
-      redoStack.add(List<Color>.from(pixelColors));
-      pixelColors = undoStack.removeLast();
+    if (_undoStack.isNotEmpty) {
+      _redoStack.add(List<Color>.from(_pixelColors));
+      _pixelColors = _undoStack.removeLast();
       notifyListeners();
     }
   }
 
   void redo() {
-    if (redoStack.isNotEmpty) {
-      undoStack.add(List<Color>.from(pixelColors));
-      pixelColors = redoStack.removeLast();
+    if (_redoStack.isNotEmpty) {
+      _undoStack.add(List<Color>.from(_pixelColors));
+      _pixelColors = _redoStack.removeLast();
       notifyListeners();
     }
   }
 
   void clear() {
     saveToUndo();
-    pixelColors = List.generate(_gridWidth * _gridHeight, (_) => const Color.fromARGB(0, 255, 255, 255));
+    _pixelColors = List.generate(_gridWidth * _gridHeight, (_) => const Color.fromARGB(0, 255, 255, 255));
     notifyListeners();
   }
 
@@ -72,7 +82,7 @@ class PixelCanvasController extends ChangeNotifier {
   }
 
   void setSelectedColor(Color color) {
-    selectedColor = color;
+    _selectedColor = color;
     notifyListeners();
   }
 
@@ -84,8 +94,8 @@ class PixelCanvasController extends ChangeNotifier {
     final y = (localPos.dy ~/ cellHeight).clamp(0, _gridHeight - 1);
     final index = y * _gridWidth + x;
 
-    if (pixelColors[index] != selectedColor) {
-      pixelColors[index] = selectedColor;
+    if (_pixelColors[index] != _selectedColor) {
+      _pixelColors[index] = _selectedColor;
       notifyListeners();
     }
   }
